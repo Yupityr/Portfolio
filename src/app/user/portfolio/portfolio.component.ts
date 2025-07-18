2a7c85ce895c709f99c2134871847fb89994dfe0
Yupityr <jrchchrstngithub@gmail.com>
2025-07-18 20:51:50 +0800

    more components added

--- src/app/user/portfolio/portfolio.component.ts
+++ src/app/user/portfolio/portfolio.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../common/header/header.component';
 import { ProfileComponent } from './profile/profile.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ExtrasComponent } from './extras/extras.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from '../../common/footer/footer.component';
 
 @Component({
   selector: 'app-portfolio',
-  imports: [HeaderComponent,ProfileComponent],
+  imports: [HeaderComponent,ProfileComponent,
+    ProjectsComponent,ExtrasComponent,
+    AboutComponent,FooterComponent],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
